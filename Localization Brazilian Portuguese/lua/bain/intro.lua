b547ce86cb5f79e2e1cbcb0e53093b3ebd4e7e86
Gabriel F <43124504+gabsF@users.noreply.github.com>
2020-09-10 11:15:06 -0400

    (ignora isso)

--- Localization Brazilian Portuguese/lua/bain/intro.lua
+++ Localization Brazilian Portuguese/lua/bain/intro.lua
@@ -1,4 +1,4 @@
-Hooks:Add("LocalizationManagerPostInit", "olainiciantedopayday", function(loc)
+Hooks:Add("LocalizationManagerPostInit", "olanoobdopayday", function(loc)
 	LocalizationManager:add_localized_strings({
 	-- Nomes e Descrições
 	heist_safehouse_hl = "The Safe House",--The safe house
