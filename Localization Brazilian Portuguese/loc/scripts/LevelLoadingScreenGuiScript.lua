d89fdc40d13140484aa80895b89c0c1c99dfa17e
Gabriel F <43124504+gabsF@users.noreply.github.com>
2022-02-06 11:06:30 -0400

    10.12.1

--- Localization Brazilian Portuguese/loc/scripts/LevelLoadingScreenGuiScript.lua
+++ Localization Brazilian Portuguese/loc/scripts/LevelLoadingScreenGuiScript.lua
@@ -1,6 +1,10 @@
 Hooks:PostHook(LevelLoadingScreenGuiScript,"init","PortugueseLocalizationLevelLoading",function(self)
-    local title_text = "ESPERE"
+    local title_text = "CARREGANDO"
 	if self._level_title_text then
 		self._level_title_text:set_text(title_text)
+		local _, _, w, h = self._level_title_text:text_rect()
+		self._level_title_text:set_size(w, h)
+		self._level_title_text:set_right(self._indicator:left())
+		self._level_title_text:set_center_y(self._indicator:center_y() + 2)
 	end
 end)
--- Localization Brazilian Portuguese/loc/scripts/LightLoadingScreenGuiScript.lua
+++ Localization Brazilian Portuguese/loc/scripts/LightLoadingScreenGuiScript.lua
@@ -1,8 +1,18 @@
 Hooks:PostHook(LightLoadingScreenGuiScript,"init","PortugueseLocalizationLightLoading",function(self)
-    local title_text = "ESPERE"
-	if self._level_title_text then
-		self._level_title_text:set_text(title_text)
-	else
-		self._title_text:set_text(title_text) --VOID UI fix
-	end
+    local title_text = "CARREGANDO"
+	self._title_text:set_text(title_text)
+end)
+
+Hooks:PostHook(LightLoadingScreenGuiScript,"setup","PortugueseLocalizationLightLoadingSetup",function(self)
+	local title_text = "CARREGANDO"
+	self._title_text:set_text(title_text)
+    self._title_text:set_font_size(32)
+	self._title_text:set_position(0, self._gui_tweak_data.border_pad)
+
+	local _, _, w, h = self._title_text:text_rect()
+
+	self._title_text:set_size(math.round(w), math.round(h))
+	self._indicator:set_center_y(self._title_text:center_y() - 2)
+	self._indicator:set_y(math.round(self._indicator:y()))
+	self._indicator:set_left(math.round(self._title_text:right() + 8))
 end)
